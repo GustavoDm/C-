e352b85bc770cd10a3c0f8a6330d4e30119d73da
GustavoDm <decaia@hotmail.com>
2018-09-10 16:53:10 -0300

    Add files via upload

---
+++ Ejercicio Alumno/Ejercicio 2/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Alumno
+    {
+        private int nota1;
+        private int nota2;
+        private float notaFinal;
+        public string apellido;
+        public string nombre;
+        public int legajo;
+
+
+        public Alumno(string apellido, string nombre, int legajo)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.legajo = legajo;
+        }
+        public void Estudiar(Alumno alumno, int nota1, int nota2)
+        {
+            alumno.nota1 = nota1;
+            alumno.nota2 = nota2;
+        }
+    }
+}
---
+++ Ejercicio Alumno/Ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 3;
+            
+            for(int i=0; i < size; i++)
+            {
+                Console.WriteLine("Ingrese el nombre del alumno: ");
+                
+            }
+        
+        }
+    }
+}
