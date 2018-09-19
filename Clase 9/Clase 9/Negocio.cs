4b0ec81f577573aab6b456901552dba88bb9a9c8
GustavoDm <decaia@hotmail.com>
2018-09-18 22:07:42 -0300

    Add files via upload

---
+++ Clase 9/Clase 9/Negocio.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_9
+{
+    public class Negocio
+    {
+        private PuestoAtencion caja;
+        private string nombre;
+        private Queue<Cliente> clientes;
+        
+        
+        private Negocio()
+        {
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(Puesto.Caja1);
+        }
+
+        private Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public Cliente Cliente
+        {
+            set
+            {
+                bool agregado = this + value;
+            }
+            get
+            {
+                return this.clientes.Dequeue();
+            }
+        }
+
+        public  static bool operator +(Negocio n, Cliente c)
+        {
+
+            bool seAgrego;
+            if (n == c)
+                seAgrego = false;
+            else
+            {
+                n.clientes.Enqueue(c);
+                seAgrego = true;
+            }
+            return seAgrego;
+        }
+
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+           
+           foreach(Cliente i in n.clientes)
+            {
+                if (c == i)
+                    return true;  
+            }
+            return false;
+        }
+
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            
+        }
+            
+
+    }
+}
---
+++ Clase 9/Clase 9/PuestoAtencion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Clase_9
+{
+    public enum Puesto
+    {
+        Caja1,
+        Caja2,
+    }
+    public class PuestoAtencion
+    {
+        private static int numeroActual;
+        private Puesto puesto;
+
+        private PuestoAtencion()
+        {
+            numeroActual = 0;
+        }
+
+        public PuestoAtencion(Puesto puesto) : this()
+        {
+            this.puesto = puesto;
+        }
+
+
+        public static int NumeroActual
+        {
+            get
+            {
+                numeroActual++;
+                return numeroActual;
+            }
+        }
+
+        public bool Atender(Cliente cliente)
+        {
+            if (cliente != null)
+            {
+                Random r = new Random();
+                int aleatorio = r.Next(1000, 3000);
+                Thread.Sleep(aleatorio);
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+}
